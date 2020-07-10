64379814b4f997b67e39bbe4ee8379c5de450397
dengqing <1247748612@qq.com>
2020-07-11 01:53:00 +0800

    更改请求接口

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  SERVER_URL: `https://api.dengqing.net/angular`,
+  SERVER_URL: `https://api.dengqing.net/angular/api`,
   production: true,
   useHash: true,
   hmr: false,
